34dea048a4a6509efe3ab5df5359492c1e9af9ae
karthikpatel6 <karthikkpatel06@gmail.com>
2026-07-06 17:50:48 +0530

    testing the websockets

---
+++ backend/test_ws.py
@@ -0,0 +1,20 @@
+import  asyncio
+import websockets
+import json
+
+async def test():
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3ZjhiM2I4ZS0xMGRiLTQxZTMtYjJlNC01MDY4MzQ3M2Y1NmQiLCJleHAiOjE3ODM5MzYyMDZ9.zM0AiqxR97adAB6ljRErSepZhM8ejdQrqs8ncC7DEEA"
+    chat_id = "fbafbc54-454d-4bd5-9fec-5ca9a7b63915"
+
+    uri = f"ws://127.0.0.1:8000/ws/{chat_id}?token={token}"
+
+    async with websockets.connect(uri) as ws:
+        print("Connected!")
+
+        await ws.send(json.dumps({"content": "Hello from Websocket!"}))
+        print("Message sent!")
+
+        response = await ws.recv()
+        print("Recevied:", response)
+
+asyncio.run(test())
